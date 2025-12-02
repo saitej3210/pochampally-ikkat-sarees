359caf00995f40be8705828c51881e770c482611
saitej3210 <saitejakodi3@gmail.com>
2025-12-02 10:41:52 +0530

    Allowed hosts fix

--- myshop/settings.py
+++ myshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_-)brez@sj+i*l7llg92rr$5cgq7qjsrda)lfyu$gyi)zq%u1x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
